68e1f0fabf186bae4fbf997120954fbf8bef857b
Suhail Gupta <suhailgupta03@gmail.com>
2023-03-26 14:35:51 +0530

    test: verifies codecov comment configuration

--- http/http_test.go
+++ http/http_test.go
@@ -9,4 +9,8 @@ func TestEOLProvider(t *testing.T) {
 	prodDetails, err := EOLProvider(EOLNode)
 	assert.Nil(t, err, "EOL provider must not return an error")
 	assert.Greater(t, len(prodDetails), 1)
+
+	prodDetails, err = EOLProvider("some-random-name")
+	assert.Nil(t, prodDetails)
+	assert.NotNil(t, err)
 }
