9c8c7d56932dd7e92b8b9586b2b3f3956c3e78d3
Suhail Gupta <suhailgupta03@gmail.com>
2023-04-09 16:21:33 +0530

    test: adds ecr runner test

--- runner_test.go
+++ runner_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"cuddly-eureka-/conf/initialize"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
@@ -34,6 +35,39 @@ func TestGetRepos(t *testing.T) {
 	repos = getRepos(appConstants.GitHubToken, appConstants.GitHubOwner)
 	assert.GreaterOrEqual(t, len(repos), 0)
 
+	repoYML := `name: Repository Details
+# Inside repository
+# name is mandatory
+# ecr is optional
+repository:
+  - name: virality
+    ecr: xxxx.dkr.ecr.us-east-1.amazonaws.com/ci:v2
+  - name: nvmrc_only
+    ecr: xxxx.dkr.ecr.us-east-1.amazonaws.com/cci:v3
+  - name: issue-test
+`
+	writeErr := os.WriteFile("test-repo-details.yml", []byte(repoYML), 0666)
+	if writeErr != nil {
+		fmt.Println("Failed to create test file ", writeErr.Error())
+		panic("")
+	}
+
+	preYML := os.Getenv("repo-details.yml")
+
+	os.Setenv("MATURITY_REPO_YAML", "test-repo-details.yml")
+	appConstants = initialize.GetAppConstants()
+	repos = getRepos(appConstants.GitHubToken, appConstants.GitHubOwner)
+	assert.Len(t, repos, 3)
+	assert.Empty(t, repos[2].ECR)
+	assert.Equal(t, "xxxx.dkr.ecr.us-east-1.amazonaws.com/ci:v2", repos[0].ECR)
+	assert.Equal(t, "xxxx.dkr.ecr.us-east-1.amazonaws.com/cci:v3", repos[1].ECR)
+	assert.NotEmpty(t, repos[0].Name)
+	assert.Equal(t, "virality", repos[0].Name)
+	assert.Equal(t, "issue-test", repos[2].Name)
+	assert.Equal(t, "nvmrc_only", repos[1].Name)
+
 	// Revert the old val
 	os.Setenv("SCAN_ALL_GITHUB_REPOS", pre)
+	os.Setenv("MATURITY_REPO_YAML", preYML)
+	os.Remove("test-repo-details.yml")
 }
