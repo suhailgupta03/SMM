1bf3697a32566d51c74ffab2a0a3d0d6a7566682
Suhail Gupta <suhailgupta03@gmail.com>
2023-04-23 15:43:14 +0530

    feat: adds cmd flags

--- conf/initialize/init.go
+++ conf/initialize/init.go
@@ -2,14 +2,27 @@ package initialize
 
 import (
 	"cuddly-eureka-/appconstants"
+	"flag"
 	"fmt"
 	"gopkg.in/yaml.v3"
+	"log"
 	"os"
 	"strings"
 )
 
-func config() (*appconstants.MaturityYAMLStruct, error) {
+var (
+	repo   *flag.FlagSet
+	github *flag.FlagSet
+)
+
+func config(yamlToReadFrom *string) (*appconstants.MaturityYAMLStruct, error) {
 	fileName := os.Getenv("MATURITY_REPO_YAML")
+	if yamlToReadFrom != nil {
+		// Override the bash variable with the value passed using the application flag
+		fileName = *yamlToReadFrom
+	}
+	maturityYAML := new(appconstants.MaturityYAMLStruct)
+	// If the filename exists, read everything from the YAML file
 	data, err := os.ReadFile(fileName)
 	if err != nil {
 		fmt.Println("Warning!!: Failed to read maturity repo yaml ..")
@@ -17,7 +30,6 @@ func config() (*appconstants.MaturityYAMLStruct, error) {
 		return nil, err
 	}
 
-	maturityYAML := new(appconstants.MaturityYAMLStruct)
 	if err = yaml.Unmarshal(data, maturityYAML); err != nil {
 		return nil, err
 	}
@@ -33,7 +45,9 @@ func isStageTest(stage string) bool {
 	}
 }
 
-func getFromEnv() appconstants.Constants {
+// getFromEnv returns the constants used across application. The values passed as
+// application flags are preferred over the variables exported from the shell
+func getFromEnv(flags *appconstants.ApplicationFlags) appconstants.Constants {
 	test := new(appconstants.TestConstants)
 	scanAllGitHub := false
 	if strings.ToLower(strings.TrimSpace(os.Getenv("SCAN_ALL_GITHUB_REPOS"))) == "true" {
@@ -54,20 +68,79 @@ func getFromEnv() appconstants.Constants {
 		test = nil
 	}
 	return appconstants.Constants{
-		Stage:         stage,
-		GitHubToken:   os.Getenv("TOKEN"),
-		GitHubOwner:   os.Getenv("OWNER"),
+		Stage: stage,
+		GitHubToken: func() string {
+			// TODO: Replace this existing GetOR function in util package
+			if flags != nil && len(*(*flags).GitHubToken) > 0 {
+				return *flags.GitHubToken
+			}
+			return os.Getenv("TOKEN")
+		}(),
+		GitHubOwner: func() string {
+			// TODO: Replace this existing GetOR function in util package
+			if flags != nil && len(*(*flags).GitHubOwner) > 0 {
+				return *flags.GitHubOwner
+			}
+			return os.Getenv("OWNER")
+		}(),
 		ScanAllGitHub: scanAllGitHub,
 		Test:          test,
 	}
+}
+
+// initFlags enables the command line flags for the application
+func initFlags() appconstants.ApplicationFlags {
+	repo = flag.NewFlagSet("repo", flag.ContinueOnError)
+	ymlFileName := repo.String("yml", "repo-details.yml", "(optional) input yaml file holding repository details")
+
+	github = flag.NewFlagSet("github", flag.ContinueOnError)
+	githubToken := github.String("token", "", "(optional) github token to use to scan a repository")
+	githubOwner := github.String("owner", "", "(optional) github account owner / username")
+
+	// Parse the command line flags
+	if len(os.Args) > 2 {
+		commandParser(os.Args[1:])
+	}
+
+	return appconstants.ApplicationFlags{
+		YAMLFile:    ymlFileName,
+		GitHubOwner: githubOwner,
+		GitHubToken: githubToken,
+	}
+}
+
+// commandParser recursively parses the command line flags
+func commandParser(osArgs []string) {
+	if len(osArgs) <= 1 {
+		return
+	}
+
+	switch osArgs[0] {
+	case "repo":
+		if err := repo.Parse(osArgs[1:]); err != nil {
+			log.Fatalf("error loading flags: %v", err)
+		}
+		osArgs = repo.Args()
+		break
+	case "github":
+		if err := github.Parse(osArgs[1:]); err != nil {
+			log.Fatalf("error loading flags: %v", err)
+		}
+		osArgs = github.Args()
+		break
+	}
+	commandParser(osArgs)
+
 }
 
 // GetAppConstants returns the application constants or the configuration
 // variables. If it fails to find conf.toml, it checks the ENV to find
 // the matching variables
 func GetAppConstants() appconstants.Constants {
-	c := getFromEnv()
-	d, _ := config()
+	flags := initFlags()
+	fmt.Println(flags)
+	c := getFromEnv(&flags)
+	d, _ := config(flags.YAMLFile)
 	if d != nil {
 		c.MaturityRepoDetails = &d.Repository
 	}
--- conf/initialize/init_test.go
+++ conf/initialize/init_test.go
@@ -12,15 +12,44 @@ func TestGetAppConstants(t *testing.T) {
 
 	prevStageVal := os.Getenv("STAGE")
 	os.Setenv("STAGE", "prod")
-	envVars := getFromEnv()
+	envVars := getFromEnv(nil)
 	assert.Nil(t, envVars.Test)
 	assert.False(t, envVars.ScanAllGitHub)
 
 	os.Setenv("SCAN_ALL_GITHUB_REPOS", "true")
-	envVars = getFromEnv()
+	envVars = getFromEnv(nil)
 	assert.True(t, envVars.ScanAllGitHub)
 
 	// Revert the old val
 	os.Setenv("STAGE", prevStageVal)
 	os.Setenv("SCAN_ALL_GITHUB_REPOS", "false")
+}
+
+func TestGetAppConstants2(t *testing.T) {
+
+	os.Args = []string{"...", "repo", "-yml=foobar.yml", "github", "-token=TOKEN", "-owner=OWNER"}
+	f := initFlags()
+	assert.Equal(t, "foobar.yml", *f.YAMLFile)
+	assert.Equal(t, "TOKEN", *f.GitHubToken)
+	assert.Equal(t, "OWNER", *f.GitHubOwner)
+
+	os.Args = []string{}
+	f = initFlags()
+	assert.Equal(t, "repo-details.yml", *f.YAMLFile)
+	assert.Equal(t, "", *f.GitHubToken)
+	assert.Equal(t, "", *f.GitHubOwner)
+
+	prevToken := os.Getenv("TOKEN")
+	prevOwner := os.Getenv("OWNER")
+	os.Setenv("TOKEN", "FOOBAR_TOKEN")
+	os.Setenv("OWNER", "FOOBAR_OWNER")
+	os.Args = []string{"...", "github", "-token=APP_TOKEN", "-owner=FOOBAR_OWNER"}
+	f = initFlags()
+	details := getFromEnv(&f)
+	assert.Equal(t, "APP_TOKEN", details.GitHubToken)
+	assert.Equal(t, "FOOBAR_OWNER", details.GitHubOwner)
+
+	// Set back the values
+	os.Setenv("TOKEN", prevToken)
+	os.Setenv("OWNER", prevOwner)
 }
